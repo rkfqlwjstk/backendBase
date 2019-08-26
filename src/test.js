b787c099cbe9ed353efb6bfb1e29940a190372c9
Kim Ju Young <julong01@naver.com>
2019-08-26 20:48:17 +0900

    1 chapter

---
+++ src/test.js
@@ -0,0 +1,26 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+    console.log(1);
+    const started = new Date();
+    await next();
+    next().then(() => {
+        console.log(new Date() - started + 'ms');
+    });
+});
+
+
+app.use((ctx, next) => {
+    console.log(2);
+    next();
+});
+
+
+app.use(ctx => {
+    ctx.body = 'Hello Koa';
+});
+
+app.listen(4000, () => {
+    console.log('heurm server is listening to port 4000');
+});
